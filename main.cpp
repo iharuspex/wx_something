27ede7daafebd19d37b6f67cfb36ba211486a5b9
aparaev <nabrosok-hd@yandex.ru>
2023-04-24 15:09:56 +0700

    chore: add minimal project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "simple.h"
+
+class MyApp : public wxApp {
+public:
+    virtual bool OnInit();
+};
+
+IMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit() {
+    Simple *simple = new Simple(wxT("Simple"));
+    simple->Show(true);
+
+    return true;
+}
---
+++ simple.h
@@ -0,0 +1,11 @@
+#ifndef SIMPLE_H_
+#define SIMPLE_H_
+
+#include <wx/wx.h>
+
+class Simple : public wxFrame {
+public:
+    Simple(const wxString& title);
+};
+
+#endif // !SIMPLE_H_
